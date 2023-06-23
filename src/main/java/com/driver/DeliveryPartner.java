1c94288a6cff28b7253a9b72ba38a905d709591d
Ramya <mutyamramya0101@gmail.com>
2023-06-23 15:40:26 +0530

    hey

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -1,7 +1,6 @@
 package com.driver;
 
 public class DeliveryPartner {
-
     private String id;
     private int numberOfOrders;
 
@@ -19,11 +18,16 @@ public class DeliveryPartner {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNumberOfOrders(int numberOfOrders) {
+        this.numberOfOrders = numberOfOrders;
+    }
+
     public Integer getNumberOfOrders(){
         return numberOfOrders;
     }
 
-    public void setNumberOfOrders(Integer numberOfOrders) {
-        this.numberOfOrders = numberOfOrders;
-    }
 }
